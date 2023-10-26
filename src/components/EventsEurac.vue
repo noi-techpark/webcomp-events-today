f682cc18838610d2640cc13e8b9bd22d1af53afe
Simon Dalvai <s.dalvai@noi.bz.it>
2023-10-26 13:52:46 +0200

    remove debug options

--- src/components/EventsEurac.vue
+++ src/components/EventsEurac.vue
@@ -95,7 +95,7 @@ export default {
       languages: ["en", "de", "it"],
       currentLanguage: "en",
       // set true to have 'Lorem ipsum' text as title and subtitle
-      devMode: true,
+      devMode: false,
       lorem:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     };
@@ -140,7 +140,6 @@ export default {
         "event-subtitle-single": this.options.maxEvents == 1,
         "event-subtitle-multiple": this.options.maxEvents > 1,
         "one-line-clamp": event.subTitle[this.currentLanguage].length > 0,
-        "two-line-clamp": event.subTitle[this.currentLanguage].length === 0,
       };
     },
     nextImage() {
@@ -174,7 +173,7 @@ export default {
       // to show more events during development
       // set increment to 0 before pushing
       const day = 60 * 40 * 24 * 1000;
-      const increment = 20;
+      const increment = 0;
 
       const params = new URLSearchParams([
         ["startdate", new Date().getTime()],
